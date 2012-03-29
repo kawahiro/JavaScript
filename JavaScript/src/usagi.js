71e2ed211e769991a2e61dfcd5fa7d5155a8c159
kawahiro <k.hiro.m305@gmail.com>
2012-03-29 12:25:11 +0900

    Slightly modified

--- JavaScript/src/usagi.js
+++ JavaScript/src/usagi.js
@@ -3,13 +3,13 @@ var month = 0;
 var beforenumber = 0;
 
 while (month < 12){
-	var b = number;
+  var b = number;
   number = number+beforenumber;
-	beforenumber = b;
-	month++;
-	console.log("month:" + month);
+  beforenumber = b;
+  month++;
+  console.log("month:" + month);
   console.log("number:" + number);
-	console.log("beforenumber:" + beforenumber);
-	console.log();
+  console.log("beforenumber:" + beforenumber);
+  console.log();
 	
 }
