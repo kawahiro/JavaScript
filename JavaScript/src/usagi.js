b3f4ef839fd90b217fb09bea6e4a37b5d2dcfc29
kawauchi <k.hiro.m305@gmail.com>
2012-03-29 12:17:31 +0900

    fibonachi

---
+++ JavaScript/src/usagi.js
@@ -0,0 +1,15 @@
+﻿var number = 1;
+var month = 0;
+var beforenumber = 0;
+
+while (month < 12){
+	var b = number;
+  number = number+beforenumber;
+	beforenumber = b;
+	month++;
+	console.log("month:" + month);
+  console.log("number:" + number);
+	console.log("beforenumber:" + beforenumber);
+	console.log();
+	
+}
